c06067ca47ff2ad2f8c274f48fc1fb29ec34cf92
trajanstevkovski <trajanstevkovski@gmail.com>
2026-01-23 19:52:21 +0100

    Class10 json

---
+++ G1/Class10/demo/ajax-fetch/script.js
@@ -0,0 +1,27 @@
+let obj = {
+    name: 'Trajan',
+    lastName: 'Stevkovski',
+    age:38,
+    courses: [
+        'Html',
+        'JS basic',
+        'JS advanced'
+    ],
+    address: {
+        street: 'some street',
+        number: 10
+    },
+
+    getAddress: function() {
+        return this.address;
+    }
+}
+
+let jsonObj = JSON.stringify(obj);
+
+console.log(jsonObj);
+
+let toJson = JSON.parse(jsonObj);
+
+console.log(toJson);
+console.log(toJson.name);
